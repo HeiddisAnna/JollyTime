3a618ef876caf8a6f8586b909389944212152242
Drífa Sóley Heimisdóttir <drifasoley@Drifas-MacBook-Pro-2.local>
2018-11-27 01:53:28 +0000

    calendar month pages

--- src/main/java/project/Util.java
+++ src/main/java/project/Util.java
@@ -6,34 +6,67 @@ import java.util.Calendar;
 import java.util.HashMap;
 
 public class Util {
+
+	public static Calendar calendar = Calendar.getInstance();
+	
+	public static class Year {
+		public int year;
+		public ArrayList<ArrayList<Day>> months;
+
+		public Year(int year, ArrayList<ArrayList<Day>> months) {
+			this.year = year;
+			this.months = months;
+		}
+	}
 	
 	public static class Day {
+		public int month;
+		public String monthName;
 		public int day;
-		public String name;
-
-		public Day(int day, String name) {
+		public String dayName;
+		
+		public Day(int month, String monthName, int day, String dayName) {
+			this.month = month;
+			this.monthName = monthName;
 			this.day = day;
-			this.name = name;
+			this.dayName = dayName;
 		}
 	}
 	
 	static HashMap<Integer, String> getDayNames() {
 		HashMap<Integer,String> hm = new HashMap<Integer,String>();
-		hm.put(Calendar.SUNDAY, "Sunnudagur");
-		hm.put(Calendar.MONDAY, "Mánudagur");
-		hm.put(Calendar.TUESDAY, "Þriðjudagur");
-		hm.put(Calendar.WEDNESDAY, "Miðvikudagur");
-		hm.put(Calendar.THURSDAY, "Fimmtudagur");
-		hm.put(Calendar.FRIDAY, "Föstudagur");
-		hm.put(Calendar.SATURDAY, "Laugardagur");
+		hm.put(Calendar.SUNDAY, "Sunday");
+		hm.put(Calendar.MONDAY, "Monday");
+		hm.put(Calendar.TUESDAY, "Tuesday");
+		hm.put(Calendar.WEDNESDAY, "Wednesday");
+		hm.put(Calendar.THURSDAY, "Thursday");
+		hm.put(Calendar.FRIDAY, "Friday");
+		hm.put(Calendar.SATURDAY, "Saturday");
 		
 		return hm;
 		
 	}
+	
+	public static HashMap<Integer, String> getMonthNames() {
+		HashMap<Integer, String> hm = new HashMap<Integer, String>();
+		hm.put(Calendar.JANUARY, "January");
+		hm.put(Calendar.FEBRUARY, "February");
+		hm.put(Calendar.MARCH, "March");
+		hm.put(Calendar.APRIL, "April");
+		hm.put(Calendar.MAY, "May");
+		hm.put(Calendar.JUNE, "June");
+		hm.put(Calendar.JULY, "July");
+		hm.put(Calendar.AUGUST, "August");
+		hm.put(Calendar.SEPTEMBER, "September");
+		hm.put(Calendar.OCTOBER, "October");
+		hm.put(Calendar.NOVEMBER, "November");
+		hm.put(Calendar.DECEMBER, "December");
+		
+		return hm;
+	}
 
-	public static ArrayList<Day> getMonth(int month) {
+	/*public static ArrayList<Day> getMonth(int month) {
 		ArrayList<Day> list = new ArrayList<Day>();
-		Calendar calendar = Calendar.getInstance();
 		
 	    calendar.set(Calendar.MONTH, month);
 	    // calendar.set(Calendar.DAY_OF_MONTH, 1);
@@ -47,8 +80,28 @@ public class Util {
 	    }
 		
 		return list;
-	}
+	}*/
+	
+	public static ArrayList<Day> getMonth(int month, int year) {
+		ArrayList<Day> list = new ArrayList<Day>();
+		
+		calendar.set(Calendar.YEAR, year);
+		calendar.set(Calendar.MONTH, month);
+		int maxMonth = calendar.getActualMaximum(Calendar.MONTH);
+		int maxDay = calendar.getActualMaximum((Calendar.DAY_OF_MONTH));
 
+		String monthName = getMonthNames().get(calendar.get(Calendar.MONTH));
+	
+		for (int j = 0; j < maxDay; j++) {
+			calendar.set(Calendar.DAY_OF_MONTH, j + 1);
+	    	String dayName = getDayNames().get(calendar.get(Calendar.DAY_OF_WEEK));
+	    	Util.Day day = new Util.Day(calendar.get(Calendar.MONTH), monthName, calendar.get(Calendar.DAY_OF_MONTH),dayName);
+	    	list.add(day);
+		}
+
+		return list;
+	}
+	
 }
 
 /*cal.set(Calendar.MONTH, 1);
--- src/main/webapp/WEB-INF/jsp/Calendar.jsp
+++ src/main/webapp/WEB-INF/jsp/Calendar.jsp
@@ -3,11 +3,32 @@
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@page import="java.util.ArrayList"%>
+<%@page import="java.util.HashMap"%>
 <%@page import="project.Util"%>
-<html lang="isl">
+<html lang="en">
 	<%
 
-        ArrayList<Util.Day> days = (ArrayList<Util.Day>) request.getAttribute("days");
+		String email = (String) request.getAttribute("email");
+        Integer selectedMonth = (Integer) request.getAttribute("selectedMonth");
+        Integer selectedYear = (Integer) request.getAttribute("selectedYear");
+        
+        Integer nextYear = selectedYear;
+        Integer nextMonth = selectedMonth + 1;
+        if (nextMonth > 11) {
+        	nextYear += 1;
+        	nextMonth = nextMonth % 12;
+        }
+        
+        Integer prevYear = selectedYear;
+        Integer prevMonth = selectedMonth - 1;
+        
+        if (prevMonth < 0) {
+        	prevYear -= 1;
+        	prevMonth = 11;
+        }
+        
+        String selectedMonthName = ((HashMap<Integer, String>) request.getAttribute("monthNames")).get(selectedMonth);
+        ArrayList<Util.Day> month = (ArrayList<Util.Day>) request.getAttribute("month");
     %>
     <head>
         <title>Calendar</title>
@@ -15,21 +36,28 @@
     </head>
     <body>
 		<h1>Calendar!!!</h1>
-		<h3>Hello ${name}!</h3>
 		
-		<div>
-		<% for(int i = 0; i < days.size(); i++) {
-                Util.Day day = days.get(i);
-            %>
+		
+		<span class="hidden" id="selectedYear"><%=selectedYear%></span>
+		<span class="hidden" id="selectedMonth"><%=selectedMonth%></span>
 
+		<h4 id="selectedMonthHeading"><%=selectedMonthName%></h4>
+		<h5 id="selectedYearHeading"><%=selectedYear%></h5>
 
-            <div>
-            	<span><%=day.day%></span>-
-                <span><%=day.name%></span>
-            </div>
-        <%
-        };
-        %> 
+		<div id="monthContainer">
+	        <% for(int i = 0; i < month.size(); i++) {
+	        		Util.Day day = month.get(i);
+	   			%>
+	   			<div class="day">
+	       			<span><%=day.monthName%></span>-
+	       			<span><%=day.day%></span>-
+	       			<span><%=day.dayName%></span>
+	   			</div>
+	   		 <% }; %>
+        </div>
+        <a id="nextButton" href="/index?year=<%=nextYear%>&month=<%=nextMonth%>&email=<%=email%>">Next Month</a>
+        <a id="prevButton" href="/index?year=<%=prevYear%>&month=<%=prevMonth%>&email=<%=email%>">Previous Month</a>
+        
 		</div>
 		<form action="/addFriend" method="GET">
 			<div class="AddFriend__button">
@@ -50,6 +78,10 @@
 		</form>	
 	
     	
+    	
+    	
+    	
+        <script src="/scripts/calendar.js"></script>
     </body>
 
 </html>
